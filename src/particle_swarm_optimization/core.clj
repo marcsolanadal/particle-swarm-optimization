4c572dc060dc23f61df248b97191c3ab0c4f280f
Marc Solà Nadal <ionthasMail@gmail.com>
2015-09-30 16:55:55 -0400

    Added all the functions. Not working yet.
    The algorithm is not yet working. It's not working correctly to
    adapt to the target fitness.

--- src/particle_swarm_optimization/core.clj
+++ src/particle_swarm_optimization/core.clj
@@ -8,58 +8,92 @@
 
 (def algo-params {:num-particles 10
                   :max-epoch 50
-                  :solution-space-max 100
                   :c1 2
                   :c2 2})
 
 (def problem-params {:target 50
-                     :max-inputs 3})
+                     :max-inputs 3
+                     :space-min -100
+                     :space-max 100})
 
 (defn calculate-fitness
   "Calculates the fitness of a given particle. The greater the value the further
   is the solucion to be correct"
-  [present]
-  (- (:target problem-params) (apply + present)))
+  [solution]
+  (- (:target problem-params) (apply + solution)))
 
 (defn init-particle
   "Initializes the particle with a number of inputs and a max-value for the
   search space. It returns a list with the fitness of the particle followed
   by the present solution"
   [inputs max-num]
-  (let [present (vec (repeatedly inputs #(rand-int max-num)))]
-    (list [(calculate-fitness present) present])))
+  (let [position (vec (repeatedly inputs #(rand-int max-num)))]
+    (conj
+      (vec (repeat 2 [(calculate-fitness position) position]))
+      (vec (repeat inputs 0)))))
 
-(init-particle 2 100)
-
-(defn new-position
+(defn update-position
   ""
-  [p gbest]
-  (let [present (last (first p))
-        pbest (last (sort-by first p))]
+  [[[current-fitness position] [best-fitness pbest] v] [_ gbest]]
 
-    ;;(+ present velocity)
-    ;;(def velocity (+ (+ (* (:c1 algo-params) (rand)) (- pbest present))
-    ;;                 (+ (* (:c2 algo-params) (rand)) (- gbest present))))
+  ;;(println "gbest:" gbest)
+  ;;(println "pbest:" pbest)
 
-    (def velocity (+ (* (:c1 algo-params) (rand)) (- pbest present)))
+  ;; Calculate the velocity differential and add it to the position
+  ;; TODO: Something is wrong here
+  (let [dv (mapv +
+                 (mapv #(* (* (:c1 algo-params) (rand)) %)
+                         (mapv - pbest position))
+                 (mapv #(* (* (:c2 algo-params) (rand)) %)
+                       (mapv - gbest position)))
+        v (mapv + v dv)
+        pos (mapv + position v)
+        new-fitness (calculate-fitness pos)]
 
-    (println present pbest velocity)
-))
+    ;;(println "dv:" dv)
+    ;;(println "v:" v)
+    ;;(println "pos:" pos)
+    ;;(println "new-fitness:" new-fitness)
+    ;;(println "best-fitness:" best-fitness)
 
-(let [particles (repeatedly 2 #(init-particle 3 100))]
+    ;;(println "new particle:"
+    ;;         [[new-fitness pos]
+    ;;          (if (> new-fitness best-fitness)
+    ;;            [new-fitness pos]
+    ;;            [current-fitness position])
+    ;;          v])
 
-  (println "particles: " particles)
+    ;; Creating new particle vector
+    [[new-fitness pos]
+     (if (> new-fitness best-fitness)
+       [new-fitness pos]
+       [current-fitness position])
+     v]
 
-  (loop [epoch 0]
-    (if (< epoch 2)
-      (let [gbest (last (last (last (sort-by first particles))))]
+    ))
 
-        (println "epoch" epoch "gbest:" gbest)
+(defn move-particles
+  ""
+  [particles gbest]
+  ;;(println particles)
+  (loop [n 0 new-particles []]
+    ;;(println (nth particles n))
+    ;;(println "iteration:" n "particles:" new-particles)
+    (if (< n (count particles))
+      (recur (inc n)
+             (conj new-particles
+                   (update-position (nth particles n) gbest)))
+      new-particles
+      )))
 
-        ;; For each particle we calculate the velocity and new position
-        (doseq [p particles]
-          (println p))
 
-        (recur (inc epoch))))))
+(loop [epoch 0
+       particles (repeatedly 2 #(init-particle 2 20))]
+  ;;(println "particles" particles)
+  (let [[gbest] (last (sort-by first particles))]
+    ;;(println "epoch:" epoch "gbest:" gbest)
+    (if (< epoch 10)
+      (recur (inc epoch) (move-particles particles gbest))
+      (println gbest))))
 
 
